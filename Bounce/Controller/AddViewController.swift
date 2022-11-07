e76ad3efa4488dfd93e73fbe01c42e7bf7880b1f
doeunkwon <bkwon38@gmail.com>
2022-11-07 09:42:30 -0800

    make Add labels match slider value

--- Bounce/Controller/AddViewController.swift
+++ Bounce/Controller/AddViewController.swift
@@ -9,23 +9,57 @@ import UIKit
 
 class AddViewController: UIViewController {
     
-    @IBOutlet weak var addLabel: UILabel!
     @IBOutlet weak var eventTextField: UITextField!
-    @IBOutlet weak var durationLabel: UILabel!
-    @IBOutlet weak var daytimeLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var timeSlider: UISlider!
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var daySlider: UISlider!
     @IBOutlet weak var addButton: UIButton!
-    @IBOutlet weak var durationSlider: UISlider!
-    @IBOutlet weak var daytimeSlider: UISlider!
+    
+    
+    var timeValue = 3
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        addLabel.text = "Add"
         
         let attributes = [NSAttributedString.Key.font: UIFont(name: "ArialRoundedMTBold", size: 25.0)]
     
         addButton.setAttributedTitle(NSAttributedString(string: "Add", attributes: attributes as [NSAttributedString.Key : Any]), for: .normal)
         
+    }
+    
+    @IBAction func timeSlide(_ sender: UISlider) {
+        timeValue = Int(sender.value)
+        timeLabel.text = "\(timeValue)h"
+    }
+    
+    @IBAction func daySlide(_ sender: UISlider) {
+        var dayValue = Int(sender.value)
+        adjustDayLabel(dayValue)
+    }
+    
+    func adjustDayLabel(_ dayValue: Int) {
+        switch dayValue {
+        case 0:
+            dayLabel.text = "early"
+        case 1:
+            dayLabel.text = "noon"
+        case 2:
+            dayLabel.text = "late"
+        default:
+            dayLabel.text = "noon"
+        }
+    }
+    
+}
+
+//MARK: - UITextFieldDelegate
+
+extension AddViewController: UITextFieldDelegate {
+    
+    // get rid keyboard when user taps elsewhere
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        eventTextField.endEditing(true)
     }
     
 }
